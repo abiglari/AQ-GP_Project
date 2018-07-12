40ec9fce58b8b005a1bc752d37780a672eebe9a6
Amir Biglari <amir.biglari@utah.edu>
2018-07-12 13:50:32 -0600

    Updating reaqdQueryFile.m.

--- matlab/readQueryFile.m
+++ matlab/readQueryFile.m
@@ -1,4 +1,4 @@
-function [lats,longs,times,pm2_5] = readQueryFile(fileName, neglectMissing)
+function [lats,longs,times,pm2_5,IDs,models] = readQueryFile(fileName, neglectMissing)
 
 if (nargin<2)
   neglectMissing = false;
@@ -24,9 +24,15 @@ longsTxt = longsTxt{1}(3:end);
 
 lats  = zeros(length(latsTxt),1);
 longs = zeros(length(longsTxt),1);
+remCols = [];
 for i=1:length(latsTxt)
   lats(i)  = str2double(latsTxt{i});
   longs(i) = str2double(longsTxt{i});
+  for j=1:i-1
+    if lats(i)==lats(j) && longs(i)==longs(j)
+      remCols = [remCols,i];
+    end
+  end
 end
   
 nt = nr-4;
@@ -34,44 +40,73 @@ pm2_5 = zeros(nt,nIDs);
 times = zeros(nt,1);
 for r=5:nr
   row = textscan(rows{r},'%q','delimiter', ',');
-  date =  datetime(row{1}{1},'InputFormat','uuuu-MM-dd''T''HH:mm:ss''Z','TimeZone','America/Denver');
+  date =  datetime(row{1}{1},'InputFormat','uuuu-MM-dd''T''HH:mm:ss''Z');
   if r==5
     startDate = date;
   end
   times(r-4) = (date - startDate) / hours(1);
   txts = row{1}(3:end);
   for c=1:length(txts)
-    if (isempty(txts{c}))
+    if (isempty(txts{c}) || str2double(txts{c})>300)
       pm2_5(r-4,c) = -1;
     else
-      pm2_5(r-4,c) = calibrate(str2double(txts{c}),models{c});
+      pm2_5(r-4,c) = str2double(txts{c});
     end
+  end
+end
+
+% pm2_5(:,remCols) = [];
+% models(remCols)  = [];
+% IDs(remCols)     = [];
+% lats(remCols)    = [];
+% longs(remCols)   = [];
+% nIDs = length(IDs);
+
+j=1;
+while j<=nIDs
+  if sum(pm2_5(pm2_5(:,j)>0,j)==pm2_5(find(pm2_5(:,j)>0,1),j)) == sum(pm2_5(:,j)>0)
+    pm2_5 = [pm2_5(:,1:j-1),pm2_5(:,j+1:end)];
+    models = [models(1:j-1);models(j+1:end)];
+    IDs = [IDs(1:j-1);IDs(j+1:end)];
+    lats = [lats(1:j-1);lats(j+1:end)];
+    longs = [longs(1:j-1);longs(j+1:end)];        
+    nIDs = nIDs-1;
+  else
+    j = j+1;
   end
 end
 
 if (~neglectMissing)
   for j=1:nIDs
     if sum(pm2_5(:,j)<=0)~=0
-      for i=1:nt
+      i =0;
+      while i<nt
+        i = i+1;
         if pm2_5(i,j)<=0 && i==1
-          pm2_5(i,j) = pm2_5(find(pm2_5(:,j)>0,1),j);
+          ind = find(pm2_5(:,j)>0,1);
+          pm2_5(1:ind-1,j) = NaN;
+          i = ind-1;
         elseif pm2_5(i,j)<=0
           z=i+1;
           while z<=nt && pm2_5(z,j)<=0
             z=z+1;
           end
-          if z<=nt
-            pm2_5(i,j) = pm2_5(i-1,j) + (pm2_5(z,j)-pm2_5(i-1,j))/(z-(i-1));
-          else
-            for tmp=i:nt
-               pm2_5(tmp,j) = pm2_5(i-1,j);
+          if (z<=nt && (z-i)<=6)
+            while (i<z)
+              pm2_5(i,j) = pm2_5(i-1,j) + (pm2_5(z,j)-pm2_5(i-1,j))/(z-(i-1));
+              i=i+1;
             end
-            break;
+          else
+            pm2_5(i:z-1,j) =NaN;
           end
+          i=z;
         end
       end
     end
   end
 end
 
+for c=1:nIDs
+  pm2_5(:,c) = calibrate(pm2_5(:,c),models{c});
+end
 end
